52cd2bd059efd943826a1a0a5aecf83cfe217b16
artred02 <clement.lardemelle@gmail.com>
2025-03-07 14:18:49 +0100

    ajout d'un commentaire et affichage des notes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { decode } from "blurhash"; // Importer la fonction decode de la bibliothèque blurhash
+import { decode } from "blurhash";
 
 const Menu = () => {
   interface MenuItem {
@@ -10,6 +10,7 @@ const Menu = () => {
     filtres?: string[];
     blurhash: string;
     image: string;
+    moyenne: number;
   }
 
   const [menuItems, setMenuItems] = useState<{ [key: string]: MenuItem[] }>({});
@@ -19,6 +20,38 @@ const Menu = () => {
   const [error, setError] = useState("");
   const [filters, setFilters] = useState<string[]>([]);
   const [imageLoaded, setImageLoaded] = useState<{ [key: number]: boolean }>({});
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [currentItemId, setCurrentItemId] = useState<number | null>(null);
+  const [formData, setFormData] = useState({ nom: "", prenom: "", contenu: "", evaluation: 5 });
+
+  const addComment = (id: number, nom: string, prenom: string, contenu: string, evaluation: number) => {
+    fetch("https://backoffice.artred02.fr/api/comment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "product_id": id,
+        "commentaire": contenu,
+        "prenom": prenom,
+        "nom": nom,
+        "evaluation": evaluation
+    }),
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Erreur lors de l'ajout du commentaire");
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // Fermer la modale après l'envoi
+        setShowModal(false);
+        setFormData({ nom: "", prenom: "", contenu: "", evaluation: 5 });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   useEffect(() => {
     fetch("https://backoffice.artred02.fr/api/getProducts")
@@ -38,7 +71,6 @@ const Menu = () => {
       });
   }, []);
 
-  // Mise à jour des filtres en fonction de la catégorie active
   useEffect(() => {
     if (activeCategory && menuItems[activeCategory]) {
       const allFilters = new Set(["Tous"]);
@@ -51,7 +83,6 @@ const Menu = () => {
     }
   }, [activeCategory, menuItems]);
 
-  // Filtrage des éléments de la catégorie active
   const filteredItems =
     activeCategory && menuItems[activeCategory]
       ? menuItems[activeCategory].filter(
@@ -59,7 +90,6 @@ const Menu = () => {
         )
       : [];
 
-  // Fonction pour générer l'image floue à partir du blurhash
   const generateBlurhashImage = (blurhash: string, width = 32, height = 32) => {
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
@@ -71,7 +101,7 @@ const Menu = () => {
       throw new Error("Failed to get 2D context");
     }
     const imageData = context.createImageData(width, height);
-    
+
     for (let i = 0; i < pixels.length; i += 4) {
       imageData.data[i] = pixels[i]; // R
       imageData.data[i + 1] = pixels[i + 1]; // G
@@ -79,7 +109,7 @@ const Menu = () => {
       imageData.data[i + 3] = 255; // Alpha (opaque)
     }
     context.putImageData(imageData, 0, 0);
-    
+
     return canvas.toDataURL("image/png");
   };
 
@@ -93,7 +123,6 @@ const Menu = () => {
           <h2 className="text-4xl font-bold mb-4 text-stone-900 dark:text-stone-100">Notre Menu</h2>
         </div>
 
-        {/* Boutons des catégories */}
         <div className="flex flex-wrap justify-center gap-2 mb-6">
           {Object.keys(menuItems).map((category) => (
             <button
@@ -113,7 +142,6 @@ const Menu = () => {
           ))}
         </div>
 
-        {/* Filtres */}
         {activeCategory && filters.length > 1 && (
           <div className="flex justify-center mb-6">
             <select
@@ -130,7 +158,6 @@ const Menu = () => {
           </div>
         )}
 
-        {/* Affichage des produits */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {filteredItems.map((item) => (
             <div
@@ -138,17 +165,15 @@ const Menu = () => {
               className="bg-white rounded-lg overflow-hidden shadow-md flex flex-col md:flex-row dark:bg-stone-700 dark:text-stone-100"
             >
               <div className="md:w-1/3 h-64 md:h-auto relative">
-                {/* Affichage du blurhash comme fond d'image pendant le chargement de l'image réelle */}
                 <div
                   className="absolute inset-0 bg-cover bg-center"
                   style={{
                     backgroundImage: `url(${generateBlurhashImage(item.blurhash)})`,
-                    backgroundSize: "cover", // S'assurer que le fond couvre toute la zone
-                    backgroundPosition: "center", // Centrer l'image floue
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
                   }}
                 ></div>
 
-                {/* L'image réelle est cachée jusqu'à ce qu'elle soit chargée */}
                 <img
                   src={`https://backoffice.artred02.fr/${item.image}`}
                   alt={item.name}
@@ -156,12 +181,12 @@ const Menu = () => {
                   onLoad={() => setImageLoaded((prev) => ({ ...prev, [item.id]: true }))}
                   style={{
                     visibility: imageLoaded[item.id] ? "visible" : "hidden",
-                    position: "absolute", // Positionner l'image réelle par-dessus le fond flou
+                    position: "absolute",
                     top: 0,
                     left: 0,
                     width: "100%",
                     height: "100%",
-                    objectFit: "cover", // Assurer que l'image couvre bien l'espace
+                    objectFit: "cover",
                   }}
                 />
               </div>
@@ -170,12 +195,98 @@ const Menu = () => {
                   <h3 className="text-xl font-bold mb-2">{item.name}</h3>
                   <p className="text-stone-600 mb-4 dark:text-stone-300">{item.description}</p>
                   <p className="text-lg font-bold">€{item.price?.toFixed(2)}</p>
-                </div>
+                  { item.moyenne
+                    ? <p className="text-stone-600">Note moyenne : {item.moyenne}/5</p>
+                    : <p className="text-stone-600">Pas de note</p>
+                  }
+                  </div>
+                  <button
+                    className="px-4 py-2 bg-red-700 text-white rounded mt-4"
+                    onClick={() => {
+                      setCurrentItemId(item.id);
+                      setShowModal(true);
+                    }}
+                  >
+                    Commenter
+                  </button>
               </div>
             </div>
           ))}
         </div>
       </div>
+
+      {/* Modale pour ajouter un commentaire */}
+      {showModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-96 dark:bg-stone-800 dark:text-stone-100">
+            <h3 className="text-2xl mb-4">Ajouter un Commentaire</h3>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (currentItemId !== null) {
+                  addComment(currentItemId, formData.nom, formData.prenom, formData.contenu, formData.evaluation);
+                }
+              }}
+            >
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Nom</label>
+                <input
+                  type="text"
+                  value={formData.nom}
+                  onChange={(e) => setFormData({ ...formData, nom: e.target.value })}
+                  className="w-full p-2 border rounded dark:bg-stone-700 dark:text-stone-100"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Prénom</label>
+                <input
+                  type="text"
+                  value={formData.prenom}
+                  onChange={(e) => setFormData({ ...formData, prenom: e.target.value })}
+                  className="w-full p-2 border rounded dark:bg-stone-700 dark:text-stone-100"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Commentaire</label>
+                <textarea
+                  value={formData.contenu}
+                  onChange={(e) => setFormData({ ...formData, contenu: e.target.value })}
+                  className="w-full p-2 border rounded dark:bg-stone-700 dark:text-stone-100"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Evaluation</label>
+                <select
+                  value={formData.evaluation}
+                  onChange={(e) => setFormData({ ...formData, evaluation: parseInt(e.target.value) })}
+                  className="w-full p-2 border rounded dark:bg-stone-700 dark:text-stone-100"
+                >
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                </select>
+              </div>
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                  className="px-4 py-2 bg-gray-300 rounded dark:bg-stone-600 dark:text-stone-100"
+                >
+                  Annuler
+                </button>
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-red-700 text-white rounded"
+                >
+                  Ajouter
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
